861884740672a4fd51ba82822428813b67513703
Wasiu-hub <contactbow@yahoo.com>
2024-01-28 20:44:28 +0000

    runnerup, feature and step defintion files created

---
+++ src/test/java/com/gic/feature/Search.feature
@@ -0,0 +1,9 @@
+Feature: Search
+
+  Scenario: User should be able to see List products and Add product on the Home page
+
+    Given User is on the Home page
+
+    When User clicks on Products
+
+    Then User should be able to see List products and Add product.
---
+++ src/test/java/com/gic/steps/Search.java
@@ -0,0 +1,23 @@
+package com.gic.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Search {
+    @Given("User is on the Home page")
+    public void user_am_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks on Products")
+    public void user_click_on_products() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should be able to see List products and Add product.")
+    public void user_should_be_able_to_see_list_products_and_add_product() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
